49a85ba997904ae8c7f0a16adba8b00556555cfd
Siavash Mirarab <smirarab@gmail.com>
2020-10-21 15:09:04 -0700

    update

--- combo_ROC.R
+++ combo_ROC.R
@@ -13,10 +13,18 @@ head (d)
 colnames(d)
 
 
-
 new_df <- do.call("rbind", list(d1, d2, d3, d4))
 
-new_df
+new_df =new_df[new_df$condition!="0.01",]
+new_df$conditionb=new_df$condition
+
+levels(new_df$condition) <- list("default" = "0.00", "default" ="p3c0", 
+                                 "Strict" = "0.01", 
+                                 "Strict+" = "p3c1", 
+                                 "Strict" = "0.02", 
+                                 "Strict+" = "0.05",
+                                 "Strict" = "p4c1",
+                                 "Liberal" = "p4c0" )
 
 
 
@@ -27,20 +35,53 @@ new_df
 mean_d$METHOD <- as.character(mean_d$METHOD)
 
 #create ROC plot
-p<-ggplot(new_df, aes(x=x.y/100, y=x.x/100))+
+ggplot(new_df, aes(x=x.y/100, y=x.x/100))+
   geom_point(aes(color = condition, shape = DB), size = 2)+
   geom_line(aes(linetype = METHOD, color = condition))+
   theme_bw()+
-  theme(legend.position = c(.65,.20), legend.direction = "horizontal",legend.margin=margin(t = 0.0, unit='cm'), )+
+  theme(legend.position = c(.65,.20), legend.direction = "horizontal",legend.margin=margin(t = 0.0, unit='cm'))+
   #coord_cartesian(ylim=c(0,1))+
   scale_y_continuous(breaks = c(0.0, 0.2, 0.4, 0.6, 0.8), name="True positive rate", labels = scales::percent_format(accuracy = 1), limits = c(0.0, NA))+
   scale_x_continuous(name="False positive rate", labels = scales::percent_format(accuracy = 1.0) )+
-  scale_color_brewer(palette = "Dark2", name="", labels = c("0.00","p3c1","0.01","p3c2", "0.02","p4c1", "0.05","p4c2"))+
+  scale_color_brewer(palette = "Dark2", name="")+#, labels = c("0.00","p3c1","0.01","p3c2", "0.02","p4c1", "0.05","p4c2"))+
   scale_linetype_manual(values=c(2, 1),name="", labels = c("Kraken","CONSULT"))+
   scale_shape(name="", labels = c("GTDB","Bac/Arch Kraken", "TOL"))+
   guides(colour = guide_legend(order = 3, ncol=4), linetype = guide_legend(order = 1, reverse=FALSE, label.direction = "horizontal"), shape = guide_legend(order = 2, label.direction = "horizontal"))
-p
-ggsave(plot=p, "ROC_combo.pdf",width=5,height = 4)
+ggsave("ROC_combo.pdf",width=5,height = 4)
+
+
+ggplot(new_df, aes(x=x.y/100, y=x.x/100))+
+  geom_point(aes(color = METHOD, shape = condition), size = 2)+
+  geom_line(aes(linetype = DB, color = METHOD))+
+  theme_bw()+
+  theme(legend.position = c(.63,.20), legend.direction = "horizontal",legend.margin=margin(t = 0.0, unit='cm') )+
+  #coord_cartesian(ylim=c(0,1))+
+  scale_y_continuous(breaks = c(0.0, 0.2, 0.4, 0.6, 0.8), name="True positive rate", labels = scales::percent_format(accuracy = 1), limits = c(0.0, NA))+
+  scale_x_continuous(name="False positive rate", labels = scales::percent_format(accuracy = 1.0) )+
+  scale_color_brewer(palette = "Dark2", name="", labels = c("Kraken","CONSULT"))+#, labels = c("0.00","p3c1","0.01","p3c2", "0.02","p4c1", "0.05","p4c2"))+
+  scale_linetype_manual(values=c(3, 2,1),name="", labels = c("GTDB","Bac/Arch Kraken", "TOL"))+
+  scale_shape_manual(name="",values=c(16,17,18,15,25))+
+  guides(colour = guide_legend(order = 3, ncol=4), linetype = guide_legend(order = 1, reverse=FALSE, label.direction = "horizontal"), shape = guide_legend(order = 2, label.direction = "horizontal"))
+ggsave("ROC_combo_2.pdf",width=5,height = 4)
+
+
+
+ggplot(new_df, aes(x=x.y/100, y=x.x/100))+
+  geom_point(aes(color = METHOD, shape=DB))+
+  geom_line(aes(linetype = DB, color = METHOD))+
+  theme_bw()+
+  theme(legend.position = c(.63,.20), legend.direction = "horizontal",legend.margin=margin(t = 0.0, unit='cm') )+
+  #coord_cartesian(ylim=c(0,1))+
+  scale_y_continuous(breaks = c(0.0, 0.2, 0.4, 0.6, 0.8), name="True positive rate", labels = scales::percent_format(accuracy = 1), limits = c(0.0, NA))+
+  scale_x_continuous(name="False positive rate", labels = scales::percent_format(accuracy = 1.0) )+
+  scale_color_brewer(palette = "Dark2", name="", labels= c("Kraken","CONSULT"))+#, labels = c("0.00","p3c1","0.01","p3c2", "0.02","p4c1", "0.05","p4c2"))+
+  scale_linetype_manual(values=c(3, 2, 1),name="", labels = c("GTDB","Bac/Arch Kraken", "TOL"))+
+  scale_size_manual(name="",values=c(0.5,1,1.5,2,1.5))+
+  scale_shape(name="",labels= c("GTDB","Bac/Arch Kraken", "TOL"))+
+  geom_text(aes(label=conditionb),size=2,nudge_y = -0.012,nudge_x = 0.0039)+
+  guides(colour = guide_legend(order = 3, ncol=4), linetype = guide_legend(order = 1, reverse=FALSE, label.direction = "horizontal"), shape = guide_legend(order = 2, label.direction = "horizontal"))
+ggsave("ROC_combo_3.pdf",width=5,height = 4)
+
 
 
 new_df$condition <- ifelse(new_df$METHOD == "kraken", "0.00", "p3c0")
